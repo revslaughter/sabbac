90b3d15f92ecd2ad8cff6bd687fb05902fbb7375
James Slaughter <prairie.squidman@gmail.com>
2013-03-09 23:22:38 -0600

    Changed set codes to be somewhat more user-friendly

--- card.py
+++ card.py
@@ -4,26 +4,26 @@ class Suit:
 	a list/dict that maps to the string. I dont know what
 	else they could be at the moment...
 	"""
-	def __init__(self, suitNum):
+	def __init__(self, suitChar):
 		"""
-		init with a suit Number, 0 to 5.
-		0 -> Face cards, no particular suit
-		1 -> Coins
-		2 -> Flasks
-		3 -> Sabres
-		4 -> Staves
+		init with a suit Charber, 0 to 5.
+		"0" -> Face cards, no particular suit
+		"c" -> Coins
+		"f" -> Flasks
+		"b" -> Sabres
+		"t" -> Staves
 		These are used in the Cards object
 		"""
 		self.suitDict = { 
-					0:"Face",
-					1:"Coins",
-					2:"Flasks",
-					3:"Sabres",
-					4:"Staves"}
-		if (suitNum >= 5) or (suitNum < 0):
-			raise Exception ("suitNum must be between 0 and 5!")
+					"0":"Face",
+					"c":"Coins",
+					"f":"Flasks",
+					"b":"Sabres",
+					"t":"Staves"}
+		if (suitChar >= 5) or (suitNum < 0):
+			raise Exception ("suitChar must be between 0 and 5!")
 		else:
-			self.suitCode = suitNum
+			self.suitCode = suitChar
 	def __repr__(self):
 		return self.suitDict[self.suitCode]
 
@@ -37,8 +37,8 @@ class Card:
 		else:
 			raise Exception("Provide a valid value for this card!")
 		self.cardSuit = getSuit(suit)
-	def getSuit(suitNum):
-		if (suitNum >= 0 and suitNum <= 5):
-			return theSuit = suit(suitNum)
+	def getSuit(suitChar):
+		if (suitChar in set(["0", "c", "f", "b", "t"]): 
+			return theSuit = suit(suitChar)
 		else:
-			raise Exception("Suit must be between 0 and 5!") 
+			raise Exception("Invalid Suit Code!") 
