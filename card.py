af2ec2a161c2079ece7dfc134bbf983e8c257bcb
James Slaughter <prairie.squidman@gmail.com>
2013-03-09 20:03:11 -0600

    adding dumb basic things that will probably all change over time

--- card.py
+++ card.py
@@ -1,8 +1,23 @@
+class suit():
+	def __init__(self, suitNum):
+		self.suitDict = { 
+					0:"Face",
+					1:"Coins",
+					2:"Flasks",
+					3:"Sabres",
+					4:"Staves"}
+	def __repr__(self):
+
 class card():
-	'''
+	"""
 	Cards are the atom of the game.
-	'''
+	"""
 	def __init__(self, value, suit):
-		self.cardValue = value
+		if abs(value) < 23:
+			self.cardValue = value
 		self.cardSuit = getSuit(suit)
-		
+	def getSuit(suitNum):
+		if (suitNum >= 0 and suitNum <= 5):
+			return theSuit = suit(suitNum)
+		else:
+			#throw error
